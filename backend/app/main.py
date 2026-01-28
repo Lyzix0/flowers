a52af6ba027da8d22d6378d10661e77393804e60
Maxim <maksim.v1liev@gmail.com>
2026-01-28 16:08:40 +0300

    backend add docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,8 +14,7 @@ TELEGRAM_CHAT_ID = os.getenv("CHAT_ID")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
